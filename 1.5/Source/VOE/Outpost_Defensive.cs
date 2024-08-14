32970ec527836b7d3bb7b8a77375622afcf5ced9
Taranchuk <nfhfyxer@gmail.com>
2024-08-14 22:59:34 +0300

    recompile + ambush raid removal

--- 1.5/Source/VOE/Outpost_Defensive.cs
+++ 1.5/Source/VOE/Outpost_Defensive.cs
@@ -16,7 +16,6 @@ public class Outpost_Defensive : Outpost
 {
     private static readonly Func<IncidentWorker_RaidEnemy, IncidentParms, bool> generateFaction;
 
-
     public static bool DoRaid;
 
     [PostToSetings("Outposts.Settings.DoIntercept", PostToSetingsAttribute.DrawMode.Checkbox, true)]
@@ -40,7 +39,7 @@ public class Outpost_Defensive : Outpost
     {
         if (OutpostsMod.Outposts.Any(outpost => typeof(Outpost_Defensive).IsAssignableFrom(outpost.worldObjectClass)))
             OutpostsMod.Harm.Patch(AccessTools.Method(typeof(IncidentWorker_RaidEnemy), "TryExecuteWorker"),
-                new(typeof(Outpost_Defensive), nameof(UpdateRaidTarget)));
+                new(typeof(Outpost_Defensive), nameof(InterceptRaid)));
         generateFaction = AccessTools.MethodDelegate<Func<IncidentWorker_RaidEnemy, IncidentParms, bool>>(AccessTools.Method(
             typeof(IncidentWorker_RaidEnemy),
             "TryResolveRaidFaction"));
@@ -51,104 +50,29 @@ public class Outpost_Defensive : Outpost
             ? "Outposts.MustBeArmed".Translate()
             : null;
 
-    public override float
-        ResolveRaidPoints(IncidentParms parms, float rangeMin = 0.35F, float rangeMax = 0.45F) //Defense gets turrets sandbags and is where combat pawns go
-        =>
-            base.ResolveRaidPoints(parms, rangeMin, rangeMax);
-
     public static string RequirementsString(int tile, List<Pawn> pawns) =>
         "Outposts.MustBeArmed".Translate()
            .Requirement(
                 pawns.Where(p => p.RaceProps.Humanlike).All(p => !p.WorkTagIsDisabled(WorkTags.Violent) && p.equipment.Primary is not null));
 
 
-    public static bool UpdateRaidTarget(IncidentParms parms, IncidentWorker_RaidEnemy __instance)
+    public static bool InterceptRaid(IncidentParms parms, IncidentWorker_RaidEnemy __instance)
     {
-        var defense = Find.WorldObjects.AllWorldObjects.OfType<Outpost_Defensive>()
-           .Where(outpost => outpost.PawnCount > 1)
-           .InRandomOrder()
-           .FirstOrDefault(d => d.DoIntercept && Rand.Chance(0.25f) && !DoRaid);
-        if (defense == null) return true;
-        if (parms.target is not Map targetMap) return true;
-        if (!generateFaction(__instance, parms)) return true;
-        defense.CanIntercept(parms, __instance);
+        if (!DoRaid) return true;
+        var defenses = Find.WorldObjects.AllWorldObjects.OfType<Outpost_Defensive>()
+           .Where(outpost => outpost.PawnCount > 0 && outpost.DoIntercept);
+        var combatSkills = defenses.Sum(x => (x.TotalSkill(SkillDefOf.Shooting) + x.TotalSkill(SkillDefOf.Melee)) * x.InterceptDifficultyMultiplier);
+        parms.points *= GetRaidSizeReductionFactor(combatSkills);
+        DoRaid = true;
+        __instance.TryExecute(parms);
+        DoRaid = false;
         return false;
     }
 
-    //To make the intercept raids as strong as I think they should be. I figured it also wouldnt be fair to throw ppls pawns to a meat grinder unprepared. 
-    //So made it a player choice cause there is just no way to estimate it on my side
-    public void CanIntercept(IncidentParms parms, IncidentWorker_RaidEnemy raid)
+    public static float GetRaidSizeReductionFactor(float totalSkills)
     {
-        var parmPoints = parms.points; //storing this because I want to generate with estimated points then set it back
-        parms.points = ResolveRaidPoints(parms, 0.4f, 0.50f) * InterceptDifficultyMultiplier;
-        var groupParms = IncidentParmsUtility.GetDefaultPawnGroupMakerParms(PawnGroupKindDefOf.Combat, parms, true);
-        groupParms.generateFightersOnly = true;
-        groupParms.dontUseSingleUseRocketLaunchers = true;
-        //I dont want to generate the pawns just to pass to world if declined confusing things so just using example. Hence text will say estimate
-        var enemies = PawnGroupMakerUtility.GeneratePawnKindsExample(groupParms).ToList();
-        if (!enemies.Any()) return;
-        CameraJumper.TryJumpAndSelect(this);
-        var nodeRoot = new DiaNode("Outposts.Intercept.CanIntercept".Translate(Name, parms.faction.Name, parms.target.ChangeType<Map>().Parent.LabelCap,
-            PawnUtility.PawnKindsToLineList(enemies, "  - ")));
-        var intercept = new DiaOption("Outposts.Intercept.InterceptRaid".Translate());
-        intercept.action = delegate { StartIntercept(parms, raid); };
-        intercept.resolveTree = true;
-        nodeRoot.options.Add(intercept);
-        var decline = new DiaOption("Outposts.Intercept.DontIntercept".Translate());
-        decline.action = delegate
-        {
-            parms.points = parmPoints;
-            DeclineIntercept(parms, raid);
-        };
-        decline.resolveTree = true;
-        nodeRoot.options.Add(decline);
-        var title = "Outposts.Intercept.Title".Translate(parms.faction.Name);
-        Find.WindowStack.Add(new Dialog_NodeTreeWithFactionInfo(nodeRoot, parms.faction, true, false, title));
-    }
-
-    private void StartIntercept(IncidentParms parms, IncidentWorker_RaidEnemy raid)
-    {
-        var targetMap = parms.target as Map;
-        if (!TileFinder.TryFindPassableTileWithTraversalDistance(targetMap.Tile, 2, 5, out var tile,
-                t => !Find.WorldObjects.AnyMapParentAt(t) && Find.WorldGrid.ApproxDistanceInTiles(Tile, t) <= 7f)) tile = Tile;
-        LongEventHandler.QueueLongEvent(() =>
-        {
-            var map = GetOrGenerateMapUtility.GetOrGenerateMap(tile, new(75, 1, 75), DefDatabase<WorldObjectDef>.GetNamed("VOE_AmbushedRaid"));
-            parms.target = map;
-
-            var defaultPawnGroupMakerParms = IncidentParmsUtility.GetDefaultPawnGroupMakerParms(PawnGroupKindDefOf.Combat, parms, true);
-            if (map.Parent is AmbushedRaid ambushedRaid)
-            {
-                ambushedRaid.DefensiveOutpost = this;
-                ambushedRaid.raidFaction = parms.faction;
-                ambushedRaid.raidPoints = parms.points;
-            }
-
-            defaultPawnGroupMakerParms.generateFightersOnly = true;
-            defaultPawnGroupMakerParms.dontUseSingleUseRocketLaunchers = true;
-            //Came across a minor bug here.Certian modded factions wont pass these validators. (VFEI insectoids is one but also only sometimes...?). It will eventually generate just produces 2 errors. Fixing it is pretty complicated and it doesnt actually hurt anything so dont think its a big deal
-            var enemies = PawnGroupMakerUtility.GeneratePawns(defaultPawnGroupMakerParms).ToList();
-            MultipleCaravansCellFinder.FindStartingCellsFor2Groups(map, out var playerSpot, out var enemySpot);
-            var pawns = AllPawns.Where(x => !x.IsPrisonerOfColony).InRandomOrder().Skip(1).ToList();
-            foreach (var pawn in pawns) GenSpawn.Spawn(RemovePawn(pawn), CellFinder.RandomSpawnCellForPawnNear(playerSpot, map), map, Rot4.Random);
-            foreach (var enemy in enemies) GenSpawn.Spawn(enemy, CellFinder.RandomSpawnCellForPawnNear(enemySpot, map), map, Rot4.Random);
-            var lordJob = new LordJob_AssaultColony(parms.faction, true, false);
-            LordMaker.MakeNewLord(parms.faction, lordJob, map, enemies);
-            var letterLabel = "Outposts.Letters.Intercept.Label".Translate();
-            var letterText = "Outposts.Letters.Intercept.Text".Translate(targetMap.Parent.LabelCap, Name);
-            PawnRelationUtility.Notify_PawnsSeenByPlayer_Letter(enemies, ref letterLabel, ref letterText,
-                "LetterRelatedPawnsGroupGeneric".Translate(Faction.OfPlayer.def.pawnsPlural), true);
-            Find.LetterStack.ReceiveLetter(letterLabel, letterText, LetterDefOf.PositiveEvent,
-                new LookTargets(new List<GlobalTargetInfo> { this, map.Parent }));
-            Find.TickManager.Notify_GeneratedPotentiallyHostileMap();
-        }, "GeneratingMapForNewEncounter", false, null, false);
-    }
-
-    private void DeclineIntercept(IncidentParms parms, IncidentWorker_RaidEnemy raid)
-    {
-        DoRaid = true;
-        raid.TryExecute(parms);
-        DoRaid = false;
+        float reductionFactor = (totalSkills / 200f) * 0.5f;
+        return Mathf.Min(reductionFactor, 0.5f);
     }
 
     public override IEnumerable<Gizmo> GetGizmos()
@@ -232,54 +156,6 @@ public class Outpost_Defensive : Outpost
 
         reason = "";
         return false;
-    }
-}
-
-public class AmbushedRaid : MapParent
-{
-    public Outpost_Defensive DefensiveOutpost;
-    public Faction raidFaction;
-    public float raidPoints;
-
-    public override bool ShouldRemoveMapNow(out bool alsoRemoveWorldObject)
-    {
-        if (!Map.mapPawns.FreeColonists.Any())
-        {
-            Find.LetterStack.ReceiveLetter("Outposts.Defensive.Lost.Label".Translate(), "Outposts.Defensive.Lost.Desc".Translate(),
-                LetterDefOf.NeutralEvent);
-            alsoRemoveWorldObject = true;
-            return true;
-        }
-
-        var pawns = Map.mapPawns.AllPawns.ListFullCopy();
-        if (!pawns.Any(p => p.Faction == raidFaction && !p.Downed))
-        {
-            var mapPawns = Map.PlayerPawnsForStoryteller.ToList();
-            foreach (var pawn in mapPawns)
-            {
-                pawn.DeSpawn();
-                DefensiveOutpost.AddPawn(pawn);
-            }
-
-            DefensiveOutpost.AddLoot(raidFaction, raidPoints, Map, out var loot);
-            Find.LetterStack.ReceiveLetter("Outposts.Defensive.Won.Label".Translate(), "Outposts.Defensive.Won.Desc".Translate(loot),
-                LetterDefOf.PositiveEvent);
-            raidFaction = null;
-            raidPoints = 0;
-            alsoRemoveWorldObject = true;
-            return true;
-        }
-
-        alsoRemoveWorldObject = false;
-        return false;
-    }
-
-    public override void ExposeData()
-    {
-        base.ExposeData();
-        Scribe_References.Look(ref DefensiveOutpost, "defensiveOutpost");
-        Scribe_References.Look(ref raidFaction, "raidFaction");
-        Scribe_Values.Look(ref raidPoints, "raidPoints");
     }
 }
 
